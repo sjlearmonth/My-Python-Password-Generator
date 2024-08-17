0fb5776481acd5f2dca85105b33c2c6ae706bf91
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2024-08-17 15:52:48 +0100

    Made code even shorter by removing a variable

--- task.py
+++ task.py
@@ -6,9 +6,8 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
-total_number_of_characters = nr_letters + nr_symbols + nr_numbers
 password = ""
-for index in range(0, total_number_of_characters):
+for index in range(0, nr_letters + nr_symbols + nr_numbers):
     character = random.randint(0, 2)
     if character == 0 and nr_letters > 0:
         password += letters[random.randint(0, len(letters) - 1)]
